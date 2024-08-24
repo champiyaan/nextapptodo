1b69ba5e53ddca0e25c0ac72967e15f9516885f0
Your Name <you@example.com>
2024-08-24 23:53:41 +0200

    react app pool connections

--- src/lib/db.js
+++ src/lib/db.js
@@ -11,11 +11,18 @@
 // export default pool;
 import { Pool } from "pg";
 
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: true, // Enforce SSL validation
+//   },
+// });
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: true, // Enforce SSL validation
-  },
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: true }
+      : { rejectUnauthorized: false },
 });
 
 export default pool;
